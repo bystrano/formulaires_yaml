82d2671a7c5b720348f51f0b3789a1a733f1033b
Michel Bystranowski <bystrano@gmx.ch>
2013-08-25 08:25:22 +0200

    nettoie, met les pipelines par ordre d'execution + debug

--- formulaires_yaml_pipelines.php
+++ formulaires_yaml_pipelines.php
@@ -11,12 +11,36 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
+function formulaires_yaml_formulaire_charger ($flux) {
+
+  $form = $flux['args']['form'];
+
+  if (preg_match('/_yaml$/', $form)) {
+
+    $nom_formulaire = substr($form, 0, -5);
+
+    // récupérer le tableau des saisies
+    include_spip('inc/yaml');
+    $saisies = yaml_decode(
+                 recuperer_fond('formulaires/'.$nom_formulaire.'_yaml',
+                                array('recuperer_saisies' => TRUE)));
+
+    $flux['data'] = array(
+                      'saisies'  => $saisies,
+                      'nom_form' => $nom_formulaire,
+                    );
+  }
+
+  return $flux;
+}
+
 function formulaires_yaml_styliser ($flux) {
 
   $fond = $flux['args']['fond'];
 
   /* si on est en train de calculer un formulaire _yaml, sauf si on est
-     en train de récupérer le tableau des saisies. */
+     en train de récupérer le tableau des saisies (sinon on fait une
+     boucle infinie). */
   if ((preg_match('/^formulaires.*_yaml$/', $fond) == 1)
       &&  ( ! $flux['args']['contexte']['recuperer_saisies'])) {
 
@@ -24,32 +48,12 @@ function formulaires_yaml_styliser ($flux) {
     $nom_formulaire = substr($fond, 12, -5);
     include_spip('balise/formulaire_');
     if (existe_formulaire($nom_formulaire)) {
-      $fond_formulaire = 'formulaires/ '.$nom_formulaire;
+      $fond_formulaire = 'formulaires/'.$nom_formulaire;
     } else { // sinon on prend le squelette par défaut
       $fond_formulaire = 'formulaires/yaml_defaut';
     }
 
-    $flux['data'] = substr(find_in_path($fond_formulaire . '.html'), 0, -5);
-  }
-
-  return $flux;
-}
-
-function formulaires_yaml_formulaire_charger ($flux) {
-
-  if (preg_match('/_yaml$/', $flux['args']['form'])) {
-
-    $nom_formulaire = substr($flux['args']['form'], 0, -5);
-
-    // récupérer le tableau des saisies
-    include_spip('inc/yaml');
-    $saisies = yaml_decode(recuperer_fond('formulaires/'.$nom_formulaire.'_yaml',
-                                          array('recuperer_saisies' => TRUE)));
-
-    $flux['data'] = array(
-                          'saisies'  => $saisies,
-                          'nom_form' => $nom_formulaire,
-                          );
+    $flux['data'] = substr(find_in_path($fond_formulaire.'.html'), 0, -5);
   }
 
   return $flux;
